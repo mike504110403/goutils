9f61cddf201fd349eb322cdc30b70c139458e1c1
A-Ze <mike504110403@gmail.com>
2024-06-16 01:19:40 +0800

    撈資料模組

---
+++ sqlLib/go.mod
@@ -0,0 +1,3 @@
+module github.com/mike504110403/sqlLib
+
+go 1.22.3
---
+++ sqlLib/sqlLib.go
@@ -0,0 +1,9 @@
+package sqllib
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	sq "github.com/Masterminds/squirrel"
+)
+
